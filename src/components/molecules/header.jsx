87730eec5ea60a7e4c7abb76d7b9796e238ce14c
Kawasaki <dokyumentopython@gmail.com>
2024-09-14 18:30:11 +0900

    ヘッダーの編集

--- src/components/molecules/header.jsx
+++ src/components/molecules/header.jsx
@@ -13,7 +13,6 @@ export const Header = () => {
                 <Nav.Link href="/login">ログイン</Nav.Link>
                 <Nav.Link href="/signup">アカウント登録</Nav.Link>
                 <Nav.Link href="/user">ユーザー</Nav.Link>
-                <Nav.Link href="/matching">マッチング</Nav.Link>
             </Nav>
             </Navbar.Collapse>
         </Container>
